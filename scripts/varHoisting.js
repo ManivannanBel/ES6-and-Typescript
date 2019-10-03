32020d9318b7b7f9551309b5938d3e91d699130e
Manivannan Ramasamy <manivannan.belfazt@gmail.com>
2019-10-03 23:48:59 +0530

    var hoisting

---
+++ scripts/varHoisting.js
@@ -0,0 +1,12 @@
+"use strict";
+//Var hoisting. Any variable declared as var goes to the top of the functional block
+function greetPerson(name) {
+    if (name === 'mani') {
+        var greet = 'hello mani';
+    }
+    else {
+        var greet = 'hi there';
+    }
+    console.log(greet); //var is declared inside if statement but accessible anywhere within the function
+}
+greetPerson('mani');
