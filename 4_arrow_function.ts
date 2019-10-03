3b07268579ed53b1ccc4f672875e7f378ab94f66
Manivannan Ramasamy <manivannan.belfazt@gmail.com>
2019-10-04 00:21:27 +0530

    arrow function

---
+++ 4_arrow_function.ts
@@ -0,0 +1,13 @@
+var getRegValue = function(){
+    return 10
+}
+
+console.log(getRegValue())
+
+const getArrowFunction1 = ()=> {
+    return 10
+}
+console.log(getArrowFunction1())
+
+const getArrowFunction2 = (m)=> 10 * m;
+console.log(getArrowFunction2(5))
---
+++ scripts/4_arrow_function.js
@@ -0,0 +1,11 @@
+"use strict";
+var getRegValue = function () {
+    return 10;
+};
+console.log(getRegValue());
+var getArrowFunction1 = function () {
+    return 10;
+};
+console.log(getArrowFunction1());
+var getArrowFunction2 = function (m) { return 10 * m; };
+console.log(getArrowFunction2(5));
